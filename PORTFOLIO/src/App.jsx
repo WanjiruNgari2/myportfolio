b75ae136a0aeac493ede0a7896f7e18e172ce83f
Damah <wanjirungari2@gmail.com>
2026-03-02 02:49:34 +0300

    added react to portfolio

---
+++ PORTFOLIO/src/App.jsx
@@ -0,0 +1,14 @@
+import Header  from './components/Header';
+import './index.css';  
+
+function App() {
+  return(
+    <div className="app-container" >
+          <Header />
+    </div>
+
+  );
+
+}
+
+export default App;
---
+++ PORTFOLIO/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <div className="appcontainer">
+                <h1 >Damaris </h1>
+                <button className="hamburger">☰</button>
+        </div>
+    );
+}
+
+export default Header;
